283caf93a38c5c5a78b2eba42858414a5d06f575
Gremory <nickgremory@gmail.com>
2023-02-08 22:17:55 +0200

    initial 1 lwsson

---
+++ Lesson001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson001
+{
+    class ProgramCLER
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
